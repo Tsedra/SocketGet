8fc2da2adf4bff65e3d811fb10b9dc3f1a63eb68
Tsedra <88805343+Tsedra@users.noreply.github.com>
2023-10-19 16:25:50 +0300

    Update request.h

--- request.h
+++ request.h
@@ -2,18 +2,8 @@
 #define DEF_REQUEST
 
 #include <string>
-#include <stdlib.h>
 #include <iostream>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
 #include <vector>
-#include <string.h>
-#include <assert.h>
-#include <stdio.h>
-#include <sstream>
 
 
 class Request{
